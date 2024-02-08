2eca4b7417fde8699846c252a5659f2e6144a8cd
EugeneArtes23 <104663023+EugeneArtes23@users.noreply.github.com>
2024-02-08 18:19:12 -0500

    Add files via upload

---
+++ Controllers/ValentinesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Valentines.Controllers
+{
+    public class ValentinesController : Controller
+    {
+        public IActionResult Valentines()
+        {
+            return View();
+        }
+    }
+}
